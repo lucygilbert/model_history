00124714adfa9fc9d53a6dca2243de233baa13c2
Lucy Gilbert <lucygilbert01@gmail.com>
2020-09-26 11:42:21 +0100

    Create initial testing version of RecordedModel

--- lib/model_history.rb
+++ lib/model_history.rb
@@ -1,2 +1,11 @@
 module ModelHistory
+  class RecordedModel < ActiveRecord::Base
+    after_save :record_model_changes
+
+    def record_model_changes
+      updated_history = JSON.parse(model_history)
+      updated_history[Time.now.to_i] = 'updated'
+      update_column('model_history', updated_history)
+    end
+  end
 end
